b5f76384b77ff234f86def39c1b3df959af714c3
aurelie661 <boutaurel59@gmail.com>
2023-07-25 14:25:11 +0200

    exo Film fini

---
+++ src/main/java/org/example/exercices_film/Main.java
@@ -0,0 +1,11 @@
+package org.example.exercices_film;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie movie1 = new Movie("La Haine","Mathieu Kassovitz",1995-05-31,"Drame");
+        Movie movie2 = new Movie("Avatar 2","James Cameron",2022-12-14,"'Action");
+
+        System.out.println(movie1);
+        System.out.println(movie2);
+    }
+}
---
+++ src/main/java/org/example/exercices_film/Movie.java
@@ -0,0 +1,62 @@
+package org.example.exercices_film;
+
+public class Movie {
+    /*1. Créer une classe "Film"
+      2. Ajouter les attributs suivants : titre, réalisateur, année de sortie et genre
+      3. Ajouter un constructeur, des getters et des setters pour ces attributs, ainsi
+        qu'une méthode pour afficher les informations sur le film*/
+
+    private String title;
+    private String director;
+    private int yearOfRelease;
+    private String gender;
+
+    public Movie(String title, String director, int yearOfRelease, String gender) {
+        this.title = title;
+        this.director = director;
+        this.yearOfRelease = yearOfRelease;
+        this.gender = gender;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie {" +
+                "title= ' " + title + '\'' +
+                ", director= ' " + director + '\'' +
+                ", yearOfRelease= " + yearOfRelease +
+                ", gender= ' " + gender + '\'' +
+                '}';
+    }
+}
