38e6b9d7e0c3b286b8251de1fa499c379bdd576b
aurelie661 <boutaurel59@gmail.com>
2023-07-28 12:32:57 +0200

    Démo Collection suite

--- src/main/java/org/example/collection/Collection.java
+++ src/main/java/org/example/collection/Collection.java
@@ -1,8 +1,7 @@
 package org.example.collection;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.Vector;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.*;
 
 public class Collection {
     public static void main(String[] args) {
@@ -25,10 +24,46 @@ public class Collection {
 
         // Démo pour sortedSet
         SortedSet<String> sortedSet = new TreeSet<>();
+
+        sortedSet.add("C#");
+        sortedSet.add("Python");
         sortedSet.add("JAVA");
-        sortedSet.add("JAVA"); // non ajouter car doublon
+        sortedSet.add("JAVA"); // non ajouter car doublons
         sortedSet.add("JAvA");
         sortedSet.add("JaVA");
-        System.out.println("SortedSet : "+ sortedSet);
+        System.out.println("SortedSet : "+ sortedSet);//Ajouter par ordre alphabetique
+
+        //Méthodes pour sortedSet
+        System.out.println("1. premier élèment : "+ sortedSet.first());
+        System.out.println("2. dernier élèment : " +sortedSet.last());
+        System.out.println("3. Taille du sortedSert : "+ sortedSet.size());
+        System.out.println("4. prend les élèments avant le head : " +sortedSet.headSet("JAvA"));
+
+        LinkedList<Double> linkedList = new LinkedList<>();
+        linkedList.add(12.63);
+        linkedList.add(15.25);
+        linkedList.add(12.63);
+        linkedList.add(19.99);
+        System.out.println("LinkedList : "+ linkedList);
+
+        //Méthodes pour linkedList
+        System.out.println("1. premier élèment : "+ linkedList.getFirst());
+        System.out.println("2. dernier élèment : " +linkedList.getLast());
+        System.out.println("3. Taille du linkedList : "+ linkedList.size());
+        linkedList.removeFirst();
+        System.out.println("LinkedList : "+ linkedList);
+        linkedList.removeLast();
+        System.out.println("LinkedList : "+ linkedList);
+
+
+        //Démo pour le SortedMap
+        SortedMap<String,Integer> sortedMap =new TreeMap<>();
+        sortedMap.put("JAVA",20);
+        sortedMap.put("PYTHON",10);
+        sortedMap.put("C#",15);
+        sortedMap.put("KOTLIN",18);
+
+        System.out.println("SortedMap "+ sortedMap);
+
     }
 }
