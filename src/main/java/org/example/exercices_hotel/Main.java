2614390c6fb8bc8950b74e70e6f702b88cdadcce
aurelie661 <boutaurel59@gmail.com>
2023-08-16 12:24:42 +0200

    Exo Hotel à améliorer

--- src/main/java/org/example/exercices_hotel/Main.java
+++ src/main/java/org/example/exercices_hotel/Main.java
@@ -2,6 +2,8 @@ package org.example.exercices_hotel;
 
 public class Main {
     public static void main(String[] args) {
+
         IhmConsole.StartIhm();
+        //todo retirer affichage dans les class, intanciation directement dans la class hotel, ajouter verification et changer les status, modifier la removeReservation.
     }
 }
